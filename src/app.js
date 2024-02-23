8e47195dd8d598cec5c6059ab738c3564be61124
Bitan Das <biggyansagar@gmail.com>
2024-02-23 19:40:46 +0530

    uploading email controller

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
 require("./db/conn");
+const Contributors = require("./models/contributors");
+const Registers = require("./models/registers");
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    auth: {
+        user: "palpantry7@gmail.com",
+        pass: "tvvv xism cozl egfg",
+    },
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,7 +24,7 @@ const template_path = path.join(__dirname, "../templates/views");
 const partial_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
@@ -42,66 +55,130 @@ app.get("/register", (req, res) => {
     res.render("register")
 });
 
-app.get("/thanksContributing", (req, res) => {
-    res.render("thanksContributing")
-});
-
-app.get("/thanksJoining", (req, res) => {
-    res.render("thanksJoining")
-});
-
 app.get("/register", (req, res) => {
     res.render("register")
 });
 
 app.post("/contribute", async (req, res) => {
     try {
-        const Contributors = require("./models/contributors");
+        const pincode = req.body.pincode;
+        const district = req.body.district;
+        const findPIN = await Registers.find({ pincode: pincode });
+        const findDIST = await Registers.find({ district: district });
+
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const email = req.body.email;
+        const phone = req.body.phone;
+        const address = req.body.address;
+        const city = req.body.city;
+        const state = req.body.state;
+        const country = req.body.country;
+        const dateOfDonation = req.body.dateOfDonation;
+        const timeOfDay = req.body.timeOfDay;
+        const whatToDonate = req.body.whatToDonate;
+        
+        let htmlText = `<ol>
+            <li>Name: ${firstName} ${lastName}</li>
+            <li>Email: ${email}</li>
+            <li>Phone: ${phone}</li>
+            <li>Address: ${address}</li>
+            <li>City: ${city}</li>
+            <li>Pincode: ${pincode}</li>
+            <li>District: ${district}</li>
+            <li>State: ${state}</li>
+            <li>Country: ${country}</li>
+            <li>Date: ${dateOfDonation}</li>
+            <li>Time: ${timeOfDay}</li>
+            <li>Meal: ${whatToDonate}</li>
+        </ol>`;
+
+        if (findPIN.length != 0) {
+
+            for (let index = 0; index < findPIN.length; index++) {
+                console.log(findPIN[index].email);
+
+                async function main() {
+                    const info = await transporter.sendMail({
+                        from: '"Pantry Pals" <palpantry7@gmail.com>',
+                        to: findPIN[index].email,
+                        subject: "New contributor registered.",
+                        html: htmlText,
+                    });
+                    
+                    console.log("Message sent: %s", info.messageId);
+                }
+                main().catch(console.error);
+                console.log(findPIN);
+            }
+        }
+        else if (findDIST.length != 0) {
+            for (let index = 0; index < findDIST.length; index++) {
+                console.log(findDIST[index].email);
+
+                async function main() {
+                    const info = await transporter.sendMail({
+                        from: '"Pantry Pals" <palpantry7@gmail.com>',
+                        to: findDIST[index].email,
+                        subject: "New contributor registered.",
+                        html: htmlText,
+                    });
+                    
+                    console.log("Message sent: %s", info.messageId);
+                }
+                main().catch(console.error);
+            }
+            console.log(findDIST);
+        }
+        else {
+        }
+
+
         const register1 = new Contributors({
-            firstName : req.body.firstName,
-            lastName : req.body.lastName,
-            email : req.body.email,
-            username : req.body.username,
-            phone : req.body.phone,
-            address : req.body.address,
-            city : req.body.city,
-            district : req.body.district,
-            state : req.body.state,
-            pincode : req.body.pincode,
-            country : req.body.country,
-            dateOfDonation : req.body.dateOfDonation,
-            timeOfDay : req.body.timeOfDay,
-            whatToDonate : req.body.whatToDonate
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+            city: req.body.city,
+            district: req.body.district,
+            state: req.body.state,
+            pincode: req.body.pincode,
+            country: req.body.country,
+            dateOfDonation: req.body.dateOfDonation,
+            timeOfDay: req.body.timeOfDay,
+            whatToDonate: req.body.whatToDonate
         });
 
         const registered1 = await register1.save();
         console.log("Done!");
-        res.render("/thanksContributing")
+        res.render("thanksContributing");
     } catch (error) {
+        console.log(error);
         res.send("An Error Occureed. Please Try Again!");
     }
 })
 
 app.post("/register", async (req, res) => {
     try {
-        const Registers = require("./models/registers");
         const register2 = new Registers({
-            name : req.body.name,
-            email : req.body.email,
-            username : req.body.username,
-            phone : req.body.phone,
-            address : req.body.address,
-            city : req.body.city,
-            district : req.body.district,
-            state : req.body.state,
-            pincode : req.body.pincode,
-            country : req.body.country,
+            fullname: req.body.fullname,
+            email: req.body.email,
+            username: req.body.username,
+            phone: req.body.phone,
+            address: req.body.address,
+            city: req.body.city,
+            district: req.body.district,
+            state: req.body.state,
+            pincode: req.body.pincode,
+            country: req.body.country,
         });
 
         const registered2 = await register2.save();
         console.log("Done!");
-        res.render("/thanksJoining");
+        res.render("thanksJoining");
     } catch (error) {
+        console.log(error);
         res.send("An Error Occureed. Please Try Again!");
     }
 })
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name : {
+    fullname : {
         type: String
     },
     email : {
